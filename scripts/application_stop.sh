40a9467b2ecd809462930f91c131fb3cde621d7a
Manjunath <33751912+nbms12@users.noreply.github.com>
2025-06-26 22:55:47 +0530

    Update application_stop.sh

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 #Stopping existing node servers
-echo "Stopping any existing node servers"
-pkill node
+#echo "Stopping any existing node servers"
+#pkill node
+
+
+###new code 
+#!/bin/bash
+# application_stop.sh
+echo "Stopping Node.js application"
+sudo systemctl stop node-app || \
+sudo pkill -f "node" || \
+echo "No node processes found"
+exit 0
